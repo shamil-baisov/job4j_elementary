f425dc867f85caa9effcedfbad0c6b9520475fb0
Shamil <baisovshamil09@gmail.com>
2023-09-05 15:35:55 +0300

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/Studentinfo.java
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class Studentinfo {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("Shamil Baisov");
         System.out.println("18.04.1990");
 
