4f27b088b08e9d8e8e88a28d44f500399d1ed2d1
Shamil <baisovshamil09@gmail.com>
2023-09-05 14:54:51 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main (String[] args) {
+        System.out.println("Shamil Baisov");
+        System.out.println("18.04.1990");
+
+    }
+
+}
